90880c11381d3039fe32c744e3fd9a35a7b603c4
carlitos2014 <carlitos_519@hotmail.com>
2019-02-22 16:23:14 -0500

    Avance Load File plan 40 semanas Mod Academico

--- routes/web.php
+++ routes/web.php
@@ -27,17 +27,6 @@ Route::group(['middleware'=>'auth'], function() {
 
 	Route::get('academic', 'Academic\StorageController@loadFile')->name('academic.loadfile');
 	Route::post('storage/create', 'StorageController@save');
-	Route::get('storage/{archivo}', function ($archivo) {
-		$public_path = public_path();
-		$url = $public_path.'/storage/'.$archivo;
-		//verificamos si el archivo existe y lo retornamos
-		if (Storage::exists($archivo))
-		{
-		  return response()->download($url);
-		}
-		//si no se encuentra lanzamos un error 404.
-		abort(404);
-   
-   });
+	
    
 });
