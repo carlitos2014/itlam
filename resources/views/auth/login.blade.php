acd5892e554696ca72d6ce4bf4df7bbe91bd16ac
carlitos2014 <carlitos_519@hotmail.com>
2019-05-24 12:04:03 -0500

    Cambios Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,13 @@
 
         <!-- /.login-logo -->
         <div class="login-box-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+            <p class="login-box-msg">Inicio de Secion</p>
 
             <form method="post" action="{{ url('/login') }}">
                 {!! csrf_field() !!}
 
                 <div class="form-group has-feedback {{ $errors->has('username') ? ' has-error' : '' }}">
-                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Username">
+                    <input type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Usuario">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                     @if ($errors->has('username'))
                         <span class="help-block">
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="form-group has-feedback{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <input type="password" class="form-control" placeholder="Password" name="password">
+                    <input type="password" class="form-control" placeholder="Contraseña" name="password">
                     <span class="glyphicon glyphicon-lock form-control-feedback"></span>
                     @if ($errors->has('password'))
                         <span class="help-block">
@@ -44,20 +44,20 @@
                     <div class="col-xs-8">
                         <div class="checkbox icheck">
                             <label>
-                                <input type="checkbox" name="remember"> Remember Me
+                                <input type="checkbox" name="remember"> Recordar
                             </label>
                         </div>
                     </div>
                     <!-- /.col -->
                     <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
                     </div>
                     <!-- /.col -->
                 </div>
             </form>
 
-            <a href="{{ url('/password/reset') }}">I forgot my password</a><br>
-            <a href="{{ url('/register') }}" class="text-center">Register a new membership</a>
+            <a href="{{ url('/password/reset') }}">¿Olvido su contraseña?</a><br>
+            <!-- <a href="{{ url('/register') }}" class="text-center">Register a new membership</a> -->
 
         </div>
         <!-- /.login-box-body -->
