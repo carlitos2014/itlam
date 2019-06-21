df9cb79011db87cfa13d6e0741e0180c359828cd
carlitos2014 <carlitos_519@hotmail.com>
2019-06-21 16:13:21 -0500

    Avance validacion de extenciones modulo sgsst

--- resources/views/sgsst/fields.blade.php
+++ resources/views/sgsst/fields.blade.php
@@ -1,4 +1,17 @@
+
 @include('flash::message')
+<script>
+function validarExtensionArchivo(){
+    var fileInput = document.getElementById('file');
+    var filePath = fileInput.value;
+    var allowedExtensions = /(\.pdf|\.docx)$/i;
+    if(!allowedExtensions.exec(filePath)){
+        alert('Solo se permite archivos de audio con esta extensión .jpg/.pdf/.docx/.xlsx/.xls .');
+        fileInput.value = '';
+        return false;
+    }
+}
+</script>
 
 <!-- Nombre Field -->
 <div class="form-group col-sm-6">
@@ -14,7 +27,7 @@
 
 <!-- Ruta Field -->
 	
-	@include('widgets.forms.input', ['type'=>'file', 'name'=>'ruta', 'label'=>'Archivo:
+	@include('widgets.forms.input', ['type'=>'file', 'id'=>'file', 'onchange'=>'return validarExtensionArchivo()', 'name'=>'ruta', 'label'=>'Archivo:
      <strong><em><h6>Tamaño Máximo: 2MB</h6></em></strong>','options'=>['required']]) 
 
 <!-- Submit Field -->
@@ -22,4 +35,3 @@
     {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
   <a href="{!! route('sgsst_s.index') !!}" class="btn btn-default">Cancelar</a>
 </div>
-
--- resources/views/sgsst/table.blade.php
+++ resources/views/sgsst/table.blade.php
@@ -19,7 +19,7 @@
                 {!! Form::open(['route' => ['sgsst_s.destroy', $row->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('sgsst_s.download',[$row->id]) !!}" class='btn btn-default btn-xs'><i class="fa fa-download"></i></a>
-                    <a href="{!! route('sgsst_s.show', [$row->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <!-- <a href="{!! route('sgsst_s.show', [$row->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a> -->
                     <a href="{!! route('sgsst_s.edit', [$row->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Esta Seguro?')"]) !!}
                 </div>
